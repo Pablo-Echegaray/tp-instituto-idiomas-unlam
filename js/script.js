b765341ac2362c42a9562c09384b5f9cd7a7e3e9
Pablo <pabloechegaray95@gmail.com>
2021-10-06 18:38:22 -0300

    Se modifican titulos y redirecciones

--- js/script.js
+++ js/script.js
@@ -2,8 +2,20 @@ let $buttons = document.querySelectorAll("button");
 
 $buttons.forEach((button) =>
   button.addEventListener("click", () => {
-    if (button.id == "moron") {
-      location.href = "sede_moron.html";
+    let sede = button.id;
+    switch (sede) {
+      case "moron":
+        location.href = "sede_moron.html";
+        break;
+      case "ramos":
+        location.href = "sede_ramos.html";
+        break;
+      case "san-justo":
+        location.href = "sede_sanJusto.html";
+        break;
+      case "ituz":
+        location.href = "sede_ituzaingo.html";
+        break;
     }
   })
 );
